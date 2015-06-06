16259c3cdd17393a4071e2328ffddb5cffec9fc0
Mayank Gupta <mayankg1992@gmail.com>
2015-06-06 19:23:33 +0530

    reset port

--- server.py
+++ server.py
@@ -11,9 +11,8 @@ def Welcome():
 def WelcomeToMyapp():
     return 'Welcome again to my app running on Bluemix!'
 
-port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port))
+	app.run()
 
 
 
